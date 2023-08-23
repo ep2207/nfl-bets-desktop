0bfe86cce20a5605f3cab274a9f5a5d8a639a932
ep2207 <114090390+ep2207@users.noreply.github.com>
2023-08-23 23:40:33 +0200

    created close match necessary functions

--- models/bet.py
+++ models/bet.py
@@ -11,6 +11,12 @@ class Bet:
     def __str__(self):
         return f"Bet ID {self.bet_id} on {self.team_bet_on}: Amount {self.bet_amount} $, Result {self.bet_result} $"
 
+    def calculate_gain(self, visiting_team_quote, receiving_team_quote):
+        if self.team_bet_on == "visiting_team":
+            odds = receiving_team_quote / visiting_team_quote
+        else:
+            odds = visiting_team_quote / receiving_team_quote
+        return self.bet_amount * odds
 
 def separate_bets_by_team(match):
     visiting_team_bets = []
@@ -30,10 +36,5 @@ def total_bet_amount(bets):
     return sum(bet.bet_amount for bet in bets)
 
      
-def calculate_gain(self, visiting_team_quote, receiving_team_quote):
-    if self.team_bet_on == "visiting_team":
-        odds = receiving_team_quote / visiting_team_quote
-    else:
-        odds = visiting_team_quote / receiving_team_quote
-    return self.bet_amount * odds
+
 
